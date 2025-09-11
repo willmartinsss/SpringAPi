b0f68b1d4d812f2d07faaed37ee3dba696fb2300
Renato Hortencio <renato.pinto0724@jala.university>
2025-09-11 14:56:13 -0300

    pushing spring base

---
+++ src/main/java/jala/university/ds3/Ds3Application.java
@@ -0,0 +1,13 @@
+package jala.university.ds3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ds3Application.class, args);
+    }
+
+}
---
+++ src/test/java/jala/university/ds3/Ds3ApplicationTests.java
@@ -0,0 +1,13 @@
+package jala.university.ds3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
