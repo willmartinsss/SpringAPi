3c3049957b8b29e8df86e56ddba9470e60b0ab13
willmartinsss <williammartins323@gmail.com>
2025-09-12 13:34:19 -0300

    adição do sql

---
+++ sql/UserApi.sql
@@ -0,0 +1,11 @@
+create schema UserApi;
+use UserApi;
+
+CREATE TABLE users (
+                       `id` VARCHAR(36) NOT NULL,         -- UUID (não autoincremento)
+                       `name` VARCHAR(200) NOT NULL,      -- Nome do usuário
+                       `login` VARCHAR(20) NOT NULL,      -- Login
+                       `password` VARCHAR(100) NOT NULL,  -- Senha (texto ou hash)
+                       PRIMARY KEY (`id`),
+                       UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+);
