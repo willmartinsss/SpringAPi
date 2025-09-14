3ece4541c9b596b66d0277efc7877c72478a26e9
Renato Hortencio <renato.pinto0724@jala.university>
2025-09-14 17:43:03 -0300

    minor fixes

--- src/main/java/jala/university/ds3/utils/ResponseBuilder.java
+++ src/main/java/jala/university/ds3/utils/ResponseBuilder.java
@@ -1,35 +1,34 @@
 package jala.university.ds3.utils;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.http.HttpStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
 @Component
 public class ResponseBuilder {
-    
+
     public ResponseEntity<?> buildResponse(String message, HttpStatus status) {
         Map<String, Object> response = new HashMap<>();
         response.put("message", message);
         response.put("status", status.value());
-        
+
         return new ResponseEntity<>(response, status);
     }
-    
-    public ResponseEntity<Map<String,Object>> build(String message, HttpStatus path , String status) {
+
+    public ResponseEntity<Map<String, Object>> build(String message, HttpStatus status, String path) {
         Map<String, Object> search = new HashMap<>();
         search.put("message", message);
         search.put("path", path);
         search.put("status", status.value());
-        
+
         return new ResponseEntity<>(search, status);
         //return new ResponseEntity.status(status).search(search);
     }
-    
-    public ResponseEntity<Map<String,Object>> build(String message, HttpStatus status) {
-        return build(message, null, status);
+
+    public ResponseEntity<Map<String, Object>> build(String message, HttpStatus status) {
+        return build(message, status, null);
     }
 }
